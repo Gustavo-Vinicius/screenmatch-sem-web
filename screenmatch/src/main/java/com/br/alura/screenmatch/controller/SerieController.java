bf191eda8c4ee1efb78ef6d80130e136ae32b7ff
Gustavo-Vinicius <gustavovinicimos12345@gmail.com>
2026-05-31 15:28:50 -0300

    feat(controller): adiciona controller para exposição de dados da API

--- screenmatch/src/main/java/com/br/alura/screenmatch/controller/SerieController.java
+++ screenmatch/src/main/java/com/br/alura/screenmatch/controller/SerieController.java
@@ -1,10 +1,14 @@
 package com.br.alura.screenmatch.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/series")
 public class SerieController {
 
+    @GetMapping("/series")
+    public String listarSeries() {
+        return "Lista de séries";
+    }
 }
